2dcecb7af2693687f2c31beb67655c42ec052714
dogpaw1230 <dogpaw1230@gmail.com>
2024-07-31 18:03:00 +0900

    feat:로고 이미지 추가

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import mainImg from './assets/img/mainPage/free-icon-deer-horns-6907484.png'
 
 export default function MainPage() {
 
     
     return (
-        <div>MainPage</div>
+        <div>
+            <div>
+                <img src={mainImg} alt="mainDeerImg" />
+            </div>
+        </div>
     )
 }
